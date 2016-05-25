3f8c1186f83b27b5ad74a2f1af77db546ec53263
LenneRoijmans <lroijmans@gmail.com>
2016-05-25 18:33:53 +0200

    Initial commit

---
+++ Assets/TestLocationPlugin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestLocationPlugin : MonoBehaviour
+{
+
+    public Text t;
+    AndroidJavaClass unity;
+
+    public void Start()
+    {
+        unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    }
+    public void callShareApp()
+    {
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        string tt = currentActivity.CallStatic<double>("getAccuracy").ToString();
+        t.text = tt;
+    }
+}
